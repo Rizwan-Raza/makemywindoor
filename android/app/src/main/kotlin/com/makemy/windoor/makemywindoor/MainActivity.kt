4296cff8f9efd28a6612892e37459f67713634b3
Rizwan Raza <rizwan.raza987@gmail.com>
2021-12-31 05:20:43 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/makemy/windoor/makemywindoor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.makemy.windoor.makemywindoor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:makemywindoor/screens/login.dart';
+
+void main() => runApp(const App());
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
